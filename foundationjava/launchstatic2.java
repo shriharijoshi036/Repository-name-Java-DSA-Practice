7d1cacefbb9804cb37a96be21162cd226ca68ae9
Shrihari Joshi <shriharijoshi036@gmail.com>
2026-03-19 22:43:54 +0530

    Using static keyword as object independent

---
+++ foundationjava/launchstatic2.java
@@ -0,0 +1,41 @@
+class School{
+    String studentName;
+    int id;
+
+   static String schoolName;
+   static int totalStudents;
+ static{
+    schoolName = "Saint Paul's Academy";
+    totalStudents = 0;
+ }
+ School(String studentName, int id){
+    this.studentName=studentName;
+    this.id=id;
+    System.out.println("name : " + studentName+ ", ID :" +id + ", schoolName :"+ schoolName);
+    totalStudents++;
+ }
+
+ 
+ static void showtotalStudents(){
+    System.out.println("totalStudents : " + totalStudents);
+ }
+
+ 
+ }
+
+
+    
+
+
+public class launchstatic2 {
+    public static void main(String[] args) {
+        School.showtotalStudents();
+        School stu1 = new School("Gauri", 13);
+        School stu2 = new School("Shradha", 14);
+    
+
+        School.showtotalStudents();
+        
+    }
+    
+}
